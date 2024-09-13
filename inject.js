73945f84f61147376c2d63061027bba8fae5ab13
TemporaryDev <124433429+TMPOraryStudios@users.noreply.github.com>
2024-09-13 20:03:06 +0100

    Create inject.js

---
+++ inject.js
@@ -0,0 +1,7 @@
+(function() {
+  var a = document.createElement("script");
+  a.type = "text/javascript";
+  a.src = "https://raw.githubusercontent.com/TMPOraryStudios/Discly/main/discly.js";
+  a.name = "Discly";
+  document.getElementsByTagName("head")[0].appendChild(a)
+})();
